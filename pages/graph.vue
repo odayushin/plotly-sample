8d9000ee27869b3a28b3e5d0374e56968a5932a4
odayushin <oda.yushin@gmail.com>
2021-02-22 21:26:33 +0900

    plotはdata.rowsを見るだけにして、rowsが更新されたらplotされるようにした

--- pages/graph.vue
+++ pages/graph.vue
@@ -6,41 +6,34 @@ import * as Plotly from "plotly.js-dist";
 export default {
   data() {
     return {
-      graphData: {},
+      rows: [],
     };
   },
   methods: {
     async getData() {
-      return await new Promise((resolve, reject) => {
-        Plotly.d3.csv(
-          "https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv",
-          (err, rows) => {
-            if (err) {
-              reject(err);
-            }
-            if (rows) {
-              resolve(rows);
-            }
+      const url =
+        "https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv";
+      this.rows = await new Promise((resolve, reject) => {
+        Plotly.d3.csv(url, (err, rows) => {
+          if (err) {
+            reject(err);
           }
-        );
+          if (rows) {
+            resolve(rows);
+          }
+        });
       });
     },
     unpack(rows, key) {
       return rows.map((row) => row[key]);
     },
     async plot() {
-      let rows;
-      try {
-        rows = await this.getData();
-      } catch (error) {
-        throw new Error(error);
-      }
       const trace1 = {
         type: "scatter",
         mode: "lines",
         name: "AAPL High",
-        x: this.unpack(rows, "Date"),
-        y: this.unpack(rows, "AAPL.High"),
+        x: this.unpack(this.rows, "Date"),
+        y: this.unpack(this.rows, "AAPL.High"),
         line: { color: "#17BECF" },
       };
 
@@ -48,8 +41,8 @@ export default {
         type: "scatter",
         mode: "lines",
         name: "AAPL Low",
-        x: this.unpack(rows, "Date"),
-        y: this.unpack(rows, "AAPL.Low"),
+        x: this.unpack(this.rows, "Date"),
+        y: this.unpack(this.rows, "AAPL.Low"),
         line: { color: "#7F7F7F" },
       };
 
@@ -57,8 +50,8 @@ export default {
         type: "scatter",
         mode: "lines",
         name: "AAPL close",
-        x: this.unpack(rows, "Date"),
-        y: this.unpack(rows, "AAPL.Close"),
+        x: this.unpack(this.rows, "Date"),
+        y: this.unpack(this.rows, "AAPL.Close"),
         line: { color: "#000000" },
       };
 
@@ -71,8 +64,13 @@ export default {
       Plotly.newPlot("chart", data, layout);
     },
   },
+  watch: {
+    rows: function (t2, t1) {
+      this.plot();
+    },
+  },
   mounted() {
-    this.plot();
+    this.getData();
   },
 };
 </script>
