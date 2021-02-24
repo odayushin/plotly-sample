8cd0dedb43df28a9f1d4df2176f810d3de100616
odayushin <oda.yushin@gmail.com>
2021-02-24 20:51:44 +0900

    plot関連の関数を分離

---
+++ modules/plotUtil.js
@@ -0,0 +1,55 @@
+import * as Plotly from "plotly.js-dist";
+export default {
+    async getData() {
+        const url = "https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv";
+        return await new Promise((resolve, reject) => {
+            Plotly.d3.csv(url, (err, rows) => {
+                if (err) {
+                    reject(err);
+                }
+                if (rows) {
+                    resolve(rows);
+                }
+            });
+        });
+    },
+    unpack(rows, key) {
+        return rows.map((row) => row[key]);
+    },
+    async plot(rows) {
+        const trace1 = {
+            type: "scatter",
+            mode: "lines",
+            name: "AAPL High",
+            x: this.unpack(rows, "Date"),
+            y: this.unpack(rows, "AAPL.High"),
+            line: { color: "#17BECF" },
+        };
+
+        const trace2 = {
+            type: "scatter",
+            mode: "lines",
+            name: "AAPL Low",
+            x: this.unpack(rows, "Date"),
+            y: this.unpack(rows, "AAPL.Low"),
+            line: { color: "#7F7F7F" },
+        };
+
+        const trace3 = {
+            type: "scatter",
+            mode: "lines",
+            name: "AAPL close",
+            x: this.unpack(rows, "Date"),
+            y: this.unpack(rows, "AAPL.Close"),
+            line: { color: "#000000" },
+        };
+
+        const data = [trace1, trace2, trace3];
+
+        const layout = {
+            title: "Basic Time Series",
+        };
+
+        Plotly.newPlot("chart", data, layout);
+    },
+}
--- pages/graph.vue
+++ pages/graph.vue
@@ -2,77 +2,20 @@
   <div id="chart"></div>
 </template>
 <script>
-import * as Plotly from "plotly.js-dist";
-import sample from "~/modules/sample";
-console.log(sample());
+import plotUtil from "~/modules/plotUtil";
 export default {
   data() {
     return {
       rows: [],
     };
   },
-  methods: {
-    async getData() {
-      const url =
-        "https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv";
-      this.rows = await new Promise((resolve, reject) => {
-        Plotly.d3.csv(url, (err, rows) => {
-          if (err) {
-            reject(err);
-          }
-          if (rows) {
-            resolve(rows);
-          }
-        });
-      });
-    },
-    unpack(rows, key) {
-      return rows.map((row) => row[key]);
-    },
-    async plot() {
-      const trace1 = {
-        type: "scatter",
-        mode: "lines",
-        name: "AAPL High",
-        x: this.unpack(this.rows, "Date"),
-        y: this.unpack(this.rows, "AAPL.High"),
-        line: { color: "#17BECF" },
-      };
-
-      const trace2 = {
-        type: "scatter",
-        mode: "lines",
-        name: "AAPL Low",
-        x: this.unpack(this.rows, "Date"),
-        y: this.unpack(this.rows, "AAPL.Low"),
-        line: { color: "#7F7F7F" },
-      };
-
-      const trace3 = {
-        type: "scatter",
-        mode: "lines",
-        name: "AAPL close",
-        x: this.unpack(this.rows, "Date"),
-        y: this.unpack(this.rows, "AAPL.Close"),
-        line: { color: "#000000" },
-      };
-
-      const data = [trace1, trace2, trace3];
-
-      const layout = {
-        title: "Basic Time Series",
-      };
-
-      Plotly.newPlot("chart", data, layout);
-    },
-  },
   watch: {
     rows: function () {
-      this.plot();
+      plotUtil.plot(this.rows);
     },
   },
-  mounted() {
-    this.getData();
+  async mounted() {
+    this.rows = await plotUtil.getData();
   },
 };
 </script>
